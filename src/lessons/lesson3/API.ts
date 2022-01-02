6369e5032dd26f9357f9a760a8b716690d74860b
Freiii21 <mrfreiii@gmail.com>
2022-01-02 14:23:48 +0300

    #42 lesson 3

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 
+
 const configOMB = {
-    baseURL: 'http://www.omdbapi.com',
+    baseURL: 'http://www.omdbapi.com/',
 };
-const key = '';
+const key = '9281af7a';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance.get(`?apikey=${key}&t==${title}`)
+            .then(response => response.data)
+            .catch(err => console.log(err));
     },
     searchFilmsByType: (title: string, type: string) => {
     }
